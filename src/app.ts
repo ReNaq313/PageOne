772b61d1cd1349646213dec4c98c668c3b1a3b78
Raza Naqvi <raza77191@gmail.com>
2024-07-05 00:08:00 +0530

    middlewares and global error handler setup

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import globalErrorHandler from "./middlewares/globalErrorHandler";
 
 const app = express();
 
 app.get("/", (req, res, next) => {
    res.json({ message: "response from pageone apis" });
 });
+
+//Global error handler
+app.use(globalErrorHandler);
 
 export default app;
