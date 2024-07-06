aef596a546de333c8a9750aa235881e1503f11c1
Raza Naqvi <raza77191@gmail.com>
2024-07-07 02:15:33 +0530

    setup multer

--- src/book/bookRouter.ts
+++ src/book/bookRouter.ts
@@ -1,10 +1,24 @@
+import path from "node:path";
 import express from "express";
 import { createBook } from "./bookController";
+import multer from "multer";
 
 const bookRouter = express.Router();
 
+const upload = multer({
+   dest: path.resolve(__dirname, "../../public/data/uploads"),
+   limits: { fileSize: 3e7 },
+});
+
 //routes
 // /api/books
-bookRouter.post("/", createBook);
+bookRouter.post(
+   "/",
+   upload.fields([
+      { name: "coverImage", maxCount: 1 },
+      { name: "file", maxCount: 1 },
+   ]),
+   createBook
+);
 
 export default bookRouter;
