d3e17b097ed99ea427d204235f49c60299fd88e9
Raza Naqvi <raza77191@gmail.com>
2024-07-06 18:16:01 +0530

    create user router

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import express, { NextFunction, Request, Response } from "express";
 import globalErrorHandler from "./middlewares/globalErrorHandler";
+import userRouter from "./user/userRouter";
 
 const app = express();
 
 app.get("/", (req, res, next) => {
    res.json({ message: "response from pageone apis" });
 });
+
+app.use("/api/users", userRouter);
 
 //Global error handler
 app.use(globalErrorHandler);
