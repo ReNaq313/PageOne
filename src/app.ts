26b9d3607b7307b908d7ca386fb53643ee3fa13b
Raza Naqvi <raza77191@gmail.com>
2024-07-06 18:29:08 +0530

    add register user validation

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import globalErrorHandler from "./middlewares/globalErrorHandler";
 import userRouter from "./user/userRouter";
 
 const app = express();
+app.use(express.json());
 
 app.get("/", (req, res, next) => {
    res.json({ message: "response from pageone apis" });
--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,6 +1,17 @@
 import { NextFunction, Request, Response } from "express";
+import createHttpError from "http-errors";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
+   const { name, email, password } = req.body;
+   //validation
+   if (!name || !email || !password) {
+      const error = createHttpError(400, "All fields are required");
+      return next(error);
+   }
+
+   //process
+   //response
+
    res.json({ message: "User created" });
 };
 
