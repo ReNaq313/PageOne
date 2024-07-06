19c69cb46d8b13f223f8c997ce00574971b0176a
Raza Naqvi <raza77191@gmail.com>
2024-07-06 22:10:15 +0530

    add user model

---
+++ src/user/userModel.ts
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+import { User } from "./userTypes";
+
+const userSchema = new mongoose.Schema<User>(
+   {
+      name: {
+         type: String,
+         required: true,
+      },
+      email: {
+         type: String,
+         unique: true,
+         required: true,
+      },
+      password: {
+         type: String,
+         required: true,
+      },
+   },
+   { timestamps: true }
+);
+
+//collection name: users
+export default mongoose.model<User>("User", userSchema);
